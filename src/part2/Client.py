47984a2b2a91157f7a3e3ebb608c82c2d3baa6c5
igohel <igohel@umass.edu>
2025-03-03 17:30:17 -0500

    adding code for latency for part 2

--- src/part2/Client.py
+++ src/part2/Client.py
@@ -2,6 +2,7 @@ import grpc
 import random
 import StockTrading_pb2_grpc   
 import StockTrading_pb2 
+import time
 
 # Lookup function
 def lookUp(stub,stockName):
@@ -25,40 +26,55 @@ def run():
     with grpc.insecure_channel("172.17.0.2:12949") as channel:
         stub = StockTrading_pb2_grpc.StockTradingStub(channel)
 
-        # numRequests = 6
+        numRequests = 3
+        totalLookupTIme = 0
+        totalTradeTime = 0
 
-        # for i in range(numRequests):
+        for i in range(numRequests):
 
-        # value of toss determines whether trade function will be called or lookup
-        toss = random.randint(0, 1)
-    
-        if toss == 0 :
-            # print("-----------------------------------------------------------------------")
-            stockName = random.choice(stockNameList)
-            print(f"Lookup Request for stockname {stockName}")
+            # value of toss determines whether trade function will be called or lookup
+            toss = random.randint(0, 1)
+        
+            if toss == 0 :
+                # print("-----------------------------------------------------------------------")
+                startLookup = time.time()
+                stockName = random.choice(stockNameList)
+                print(f"Lookup Request for stockname {stockName}")
+                response = lookUp(stub, stockName)
+                endLookUp = time.time()
+                latencyLookUp = endLookUp - startLookup
+                totalLookupTIme += latencyLookUp
+                if response.price == -1:
+                    print(f"Lookup Response: {stockName} is an invalid stock!")
+                else:
+                    print(f"Lookup Response: Lookup is successful for {stockName} -  Price: {response.price}, Volume: {response.volume}")
 
-            response = lookUp(stub, stockName)
+                # print(f"Latency for Lookup: {latencyLookUp}")
+            if toss == 1 :
+                # print("-----------------------------------------------------------------------")
+                startTrade = time.time()
+                stockName = random.choice(stockNameList)
+                n = random.randint(1,50)
+                type = random.choice(tradeType)
+                print(f"Trade Request for stockname {stockName}, number of items {n} and trade type {type}")
 
-            if response.price == -1:
-                print(f"Lookup Response: {stockName} is an invalid stock!")
-            else:
-                print(f"Lookup Response: Lookup is successful for {stockName} -  Price: {response.price}, Volume: {response.volume}")
-        if toss == 1 :
-            # print("-----------------------------------------------------------------------")
-            stockName = random.choice(stockNameList)
-            n = random.randint(1,50)
-            type = random.choice(tradeType)
-            print(f"Trade Request for stockname {stockName}, number of items {n} and trade type {type}")
+                response = trade(stub, stockName, n, type)
+                endTrade = time.time()
+                latencyTrade = endTrade - startTrade
+                totalTradeTime += latencyTrade
+                if response.isSuccess == 1:
+                    print(f"Trade Response: Trade is successful for {stockName} - {type} {n} stocks!")
+                elif response.isSuccess == 0:
+                    print(f"Trade Response: Trade is suspended for {stockName}")
+                elif response.isSuccess == -1:
+                    print(f"Trade Response: {stockName} is an invalid stock!")
 
-            response = trade(stub, stockName, n, type)
+                # print(f"Latency for Trade: {latencyTrade}")
 
-            if response.isSuccess == 1:
-                print(f"Trade Response: Trade is successful for {stockName} - {type} {n} stocks!")
-            elif response.isSuccess == 0:
-                print(f"Trade Response: Trade is suspended for {stockName}")
-            elif response.isSuccess == -1:
-                print(f"Trade Response: {stockName} is an invalid stock!")
-            
+        avgLatencyLookup = totalLookupTIme / numRequests
+        print(f"Avg Latency for Lookup: {avgLatencyLookup : .4f} seconds") 
+        avgLatencyTrade = totalTradeTime / numRequests
+        print(f"Avg Latency for Trade: {avgLatencyTrade : .4f} seconds")    
 
 
 if __name__ == "__main__":
--- src/part2/UpdateClient.py
+++ src/part2/UpdateClient.py
@@ -19,25 +19,33 @@ def run():
     with grpc.insecure_channel("172.17.0.2:12949") as channel:
         stub = StockTrading_pb2_grpc.StockTradingStub(channel)
 
-        # numUpdates = 10
+        numUpdates = 3
+        totalUpdateTime = 0
 
-        # for i in range(numUpdates):
-        # print("-----------------------------------------------------------------------")
-        stockName = random.choice(stockNameList)
-        price = random.randint(0,500)
-        print(f"Update Request for stockname {stockName}, price {price}")
+        for i in range(numUpdates):
+            # print("-----------------------------------------------------------------------")
+            startUpdate = time.time()
+            stockName = random.choice(stockNameList)
+            price = random.randint(0,500)
+            print(f"Update Request for stockname {stockName}, price {price}")
 
-        response = update(stub, stockName, price)
+            response = update(stub, stockName, price)
+            endUpdate = time.time()
+            latencyUpdate = endUpdate - startUpdate
+            totalUpdateTime += latencyUpdate
+            if response.isUpdated == 1:
+                print(f"Update Response: Catalog price update is successful for {stockName}. New price is {price}! ")
+            elif response.isUpdated == -1:
+                print(f"Update Response: {stockName} is an invalid stock!")
+            elif response.isUpdated == -2:
+                print(f"Update Response: {price} is an invalid!")
+            
+            # print(f"Latency for Update: {latencyUpdate}")
+            avgLatencyUpdate = totalUpdateTime / numUpdates
+            print(f"Avg Latency for Update: {avgLatencyUpdate : .4f} seconds") 
 
-        if response.isUpdated == 1:
-            print(f"Update Response: Catalog price update is successful for {stockName}. New price is {price}! ")
-        elif response.isUpdated == -1:
-            print(f"Update Response: {stockName} is an invalid stock!")
-        elif response.isUpdated == -2:
-            print(f"Update Response: {price} is an invalid!")
-        
-        # Sleep - for periodic updates to the stock prices
-        time.sleep(random.randint(1,5))
+            # Sleep - for periodic updates to the stock prices
+            time.sleep(random.randint(1,5))
 
 
 if __name__ == "__main__":
